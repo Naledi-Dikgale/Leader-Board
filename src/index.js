ab5f85dc6fbb5c3d27b98da3f955bab18156dc2c
naledi-dikgale <naledi22khosi@gmail.com>
2023-04-11 12:10:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,13 @@
-import addScore from './modules/score.js';
-
 import './style.css';
+import {createGame, createScore, displayScore} from './modules/promises.js'
+import {refresh, btn,addScore} from './modules/elements.js'
 
-const scores = [
-  {
-    name: 'Gift',
-    score: 1,
-  },
-  {
-    name: 'Thoriso',
-    score: 2,
-  },
-  {
-    name: 'TJ',
-    score: 7,
-  },
-];
 
-const displayScore = (scores) => {
-  const scoreList = document.getElementById('score-list');
-  scoreList.innerHTML = '';
-  scores.forEach((scoreListItem) => {
-    const scoreItem = document.createElement('li');
-    scoreItem.className = 'scrore-list-item';
-    scoreItem.innerHTML = `Name: ${scoreListItem.name}`;
-    scoreList.appendChild(scoreItem);
-  });
-};
+addScore.addEventListener('click',(event)=>{
+    event.preventDefault();
+    createScore();
+})
 
-window.addEventListener('load', () => {
-  displayScore(scores);
-  const refresh = document.getElementById('refresh');
-  refresh.addEventListener('click', () => {
-    displayScore(scores);
-  });
-  const addNewScore = document.getElementById('add-new-score');
-  addNewScore.addEventListener('click', (e) => {
-    e.preventDefault();
-    const inputName = document.getElementById('input-name');
-    const inputScore = document.getElementById('input-score');
-    addScore(inputName.value, inputScore.value, scores);
-    inputName.value = '';
-    inputScore.value = '';
-    displayScore(scores);
-  });
-});
+displayScore();
+refresh.onclick = displayScore;
+btn.onclick = createGame;
