645a770df9d2a315c4068064436972ec55fa0b62
naledi-dikgale <naledi22khosi@gmail.com>
2023-09-25 20:08:26 +0200

    Add clear button

--- src/modules/elements.js
+++ src/modules/elements.js
@@ -1,8 +1,9 @@
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/id:57841632/scores/';
 const refresh = document.getElementById('refresh');
+const clear = document.getElementById('clear');
 const addScore = document.getElementById('insertScore');
 const scoreList = document.getElementById('score-list');
 
 export {
-  refresh, addScore, scoreList, url,
+  refresh, clear, addScore, scoreList, url,
 };
